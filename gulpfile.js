87f3b0502b6fc6dcafe271771b8bc84e1ba72d74
Dmitrii <itdriver@yahoo.com>
2023-11-12 23:24:14 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const {
+    src,
+    dest
+} = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(concat('style.min.css'))
+        .pipe(scss({
+            outputStyle: 'compressed'
+        })) // call scss const + compression
+        .pipe(dest('app/css'))
+}
+
+exports.styles = styles;
