34dc984aeddb3d8e7f9d620b5a0c0543102f73aa
Dmitrii <itdriver@yahoo.com>
2023-11-14 00:00:26 +0300

    watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const {
     src,
-    dest
+    dest,
+    watch
 } = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
+
 function styles() {
     return src('app/scss/style.scss')
         .pipe(concat('style.min.css'))
@@ -15,4 +17,20 @@ function styles() {
         .pipe(dest('app/css'))
 }
 
-exports.styles = styles;
+function scripts() {
+    return src('app/js/main.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+}
+
+function watching() {
+    watch(['app/scss/style.scss'], styles) //chsnce in brakets - starts after comma 
+    watch(['app/js/main.js'], scripts)
+}
+
+
+
+exports.styles = styles; //.gulp command = function
+exports.scripts = scripts;
+exports.watching = watching;
